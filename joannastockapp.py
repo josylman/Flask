9c5ec0886af4993a3d478ed13ad1d578eb672b56
Joanna Sylman <jsylman@gmail.com>
2018-06-17 10:58:30 -0700

    update 4

--- joannastockapp.py
+++ joannastockapp.py
@@ -64,4 +64,4 @@ def indexplotter():
 # when there are code changes
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=port)
